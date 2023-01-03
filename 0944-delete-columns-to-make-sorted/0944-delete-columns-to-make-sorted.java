36b7ea1a1163eec88374d4c90620d755970aad11
Shrikrishnan Balakrishnan <17940323+Shrikrishnan@users.noreply.github.com>
2023-01-03 07:57:23 +0530

    Time: 18 ms (40.67%), Space: 47.4 MB (58.20%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -1,21 +1,31 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
-        int count=0;
-        char prev='a';
-        char a='a';
-        for(int i=0; i<strs[0].length(); i++){
-             a='a';
-            for(int j=0; j<strs.length;j++){
-                prev=a;
-                a=strs[j].charAt(i);
-                if(j==0) continue;
-                if(prev<=a) continue;
-                else{
+        //  Creating an cW for the Iteration of the String Coloum Wise 
+        int cW = strs[0].length();
+        // Creation of the ch for the Iteration of the Array Row Wise
+        int cH = strs.length;
+        //  declaration of the Varabile for the Counting 
+        int count =  0;
+        // First For loop  for Chacking 
+        for(int i = 0 ;i<cW ; i++){
+            // Second for loop for the Array iteration 
+            for(int j=1;j<cH;j++){
+                // By Passing the First Coloum For Checking
+                if(j==0){
+                    continue;
+                }
+                // checking if it is Lexicographically Sorted or not 
+                if(strs[j].charAt(i)>=strs[j-1].charAt(i)){
+                    continue;
+                }else{
+                // if the condition got failed 
                     count++;
                     break;
                 }
             }
         }
+        
         return count;
+        
     }
 }
